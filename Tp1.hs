0a2f9c96ebc824c63caf391629d5f22a23d4c58f
Matias Agustin Pascual <matiasapascual@gmail.com>
2019-04-18 23:20:09 -0300

    Punto 4

--- Tp1.hs
+++ Tp1.hs
@@ -27,3 +27,19 @@ incrementarVelocidad unAuto
  | (length.filter (esVocal).enamorade) unAuto < 3 = unAuto {velocidad = ((+15).velocidad) unAuto}
  | (length.filter (esVocal).enamorade) unAuto < 5 = unAuto {velocidad = ((+20).velocidad) unAuto}
  | otherwise = unAuto {velocidad =((+30).velocidad) unAuto}
+ 
+ -- PUNTO 3
+ 
+puedeUsarTruco :: Auto -> Bool
+puedeUsarTruco unAuto = (nivelNafta unAuto) > 0 && (velocidad  unAuto< 100)
+-- seguro se puede mejorar con aplicación parcial
+
+-- PUNTO 4
+comboLoco :: Auto -> Auto
+comboLoco = deReversa.nitro
+
+queTrucazo :: Auto -> String -> Auto
+queTrucazo unAuto falsoEnamorade = incrementarVelocidad (fingirAmor unAuto falsoEnamorade)
+
+turbo :: Auto -> Auto
+turbo unAuto = unAuto {nivelNafta = 0, velocidad = velocidad unAuto + round((nivelNafta unAuto) *10)} 
